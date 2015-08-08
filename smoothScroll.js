2f365b3574c5f836943b192f047211cee8f643f9
Pete Kreppein <me@kreppe.in>
2015-08-08 17:58:29 +0000

    added readme at top, cleaned up

--- smoothScroll.js
+++ smoothScroll.js
@@ -1,9 +1,29 @@
-console.log("Smooth Scroll Initialized");
-	//Stackable is a class given to navigation links on the section headers on the resume page
+// Easy Smooth Scroll jQuery plugin for scrolling between two elements on 
+// the same page.
 
-$(".stackable a, .smooth").click(function(){
+// Simply add the class .smooth to any link you want to enable this plugin for,
+// and make the target of the a element the ID on the page you want to scroll for.
+
+// Example:
+
+// <a class="smooth" href="#contact-us">Contact Us</a>
+
+// -Will scroll smoothly too-
+
+// <div id="contact-us">
+// ...
+// </div>
+
+// Feel free to remove the console.log statements, they are just for my personal
+// error checking, and to see when the script fires.
+
+// Copyright (c) 2015 Peter M Kreppein
+// Licensed under MIT License (http://opensource.org/licenses/MIT for more.)
+
+
+console.log("Smooth Scroll Initialized");
+$(".smooth").click(function(){
 	event.preventDefault();
-	
 	//Get URL of link
 	var fullUrl = this.href;
 	//split url into two parts, with # as the denoting character
@@ -13,9 +33,7 @@ $(".stackable a, .smooth").click(function(){
 	//Get offset as object, grab only top offset
 	var targetOffset = $("#" + target).offset();
 	var targetTop = targetOffset.top;
-	
+	//Animate The page between links
 	$("html, body").animate({scrollTop: targetTop}, 1700);
-	
 	console.log("Scroooooooooooolllll!");
-	
 });
